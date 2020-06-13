e6830ea75efce96706e262acf782a75b71f95de6
Masoud <m.shokrnezhad@gmail.com>
2020-06-13 17:32:20 +0430

    Adding more comment

--- main.py
+++ main.py
@@ -217,4 +217,4 @@ while running:
 			print(bcolors.FAIL + "Enemy attacked " + players[enemy_choice].name + " for " + str(enemy_damage) +
 				  " damages" + bcolors.ENDC)
 
-			#it is done for the second time.
+			#it is not working.
